907ef2d5d7c1a51e199329b3bb01d5ac3d6f112f
rinwine <rinwine@163.com>
2017-01-05 16:41:06 +0800

    Site updated: 2017-01-05 16:41:03

--- js/insight.js
+++ js/insight.js
@@ -217,7 +217,7 @@
     });
 
 
-    $(document).on('click focus', '.search-form-submit', function () {
+    $(document).on('click focus', '.search-form-input', function () {
         $main.addClass('show');
         $main.find('.ins-search-input').focus();
     }).on('click', '.ins-search-item', function () {
